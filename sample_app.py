d48ae847c35c8a71fe1b3e44122711d6b8d50343
Cindy Vo <cindyvo@example.com>
2026-01-14 10:42:20 +0000

    Fix sample_app.py and resolve merge conflict

--- sample_app.py
+++ sample_app.py
@@ -1,10 +1,7 @@
 <<<<<<< HEAD
-=======
 # Add to this file for the sample app lab
->>>>>>> 7d0d18b60c842c9d1d910e7a6cae32415100f533
-from flask import Flask
-from flask import request
-from flask import render_template
+
+from flask import Flask, request, render_template
 
 sample = Flask(__name__)
 
@@ -13,8 +10,4 @@ def main():
     return render_template("index.html")
 
 if __name__ == "__main__":
-<<<<<<< HEAD
     sample.run(host="0.0.0.0", port=5050, threaded=False)
-=======
-    sample.run(host="0.0.0.0", port=5050)
->>>>>>> 7d0d18b60c842c9d1d910e7a6cae32415100f533
